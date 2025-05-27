0458f3e5fdb01521f85af6165b2c162e7e67ec97
Anton Pozharskiy <apozharski@gmail.com>
2025-05-27 13:54:53 +0200

    move to using a custom solve_homotopy! method instead of solve!

--- src/ipoptc/regular_patch.jl
+++ src/ipoptc/regular_patch.jl
@@ -7,7 +7,150 @@ function get_eta(solver::MadNLPSolver)
     end
 end
 
-function MadNLP.regular!(
+function madnlp_homotopy(model::MadMPEC.ScholtesRelaxation; kwargs...)
+    solver = MadNLP.MadNLPSolver(model; kwargs...)
+    return solve_homotopy!(solver)
+end
+
+function solve_homotopy!(
+    nlp::MadMPEC.ScholtesRelaxation,
+    solver::MadNLP.MadNLPSolver{
+        T,
+        VT,
+        VI,
+        KKTSystem,
+        MadMPEC.ScholtesRelaxation{T, VT},
+        CB,
+        Iterator,
+        IC,
+        KKTVec,
+    };
+    kwargs...,
+) where {T, VT, VI, KKTSystem, CB, Iterator, IC, KKTVec}
+    return solve_homotopy!(nlp, solver, MadNLP.MadNLPExecutionStats(solver); kwargs...)
+end
+
+function solve_homotopy!(
+    solver::MadNLP.MadNLPSolver{
+        T,
+        VT,
+        VI,
+        KKTSystem,
+        MadMPEC.ScholtesRelaxation{T, VT},
+        CB,
+        Iterator,
+        IC,
+        KKTVec,
+    };
+    kwargs...,
+) where {T, VT, VI, KKTSystem, CB, Iterator, IC, KKTVec}
+    return solve_homotopy!(solver.nlp, solver; kwargs...)
+end
+
+function solve_homotopy!(
+    nlp::MadMPEC.ScholtesRelaxation,
+    solver::MadNLP.MadNLPSolver{
+        T,
+        VT,
+        VI,
+        KKTSystem,
+        MadMPEC.ScholtesRelaxation{T, VT},
+        CB,
+        Iterator,
+        IC,
+        KKTVec,
+    },
+    stats::MadNLP.MadNLPExecutionStats;
+    x=nothing,
+    y=nothing,
+    zl=nothing,
+    zu=nothing,
+    kwargs...,
+) where {T, VT, VI, KKTSystem, CB, Iterator, IC, KKTVec}
+    if x != nothing
+        MadNLP.full(solver.x)[1:get_nvar(nlp)] .= x
+    end
+    if y != nothing
+        solver.y[1:get_ncon(nlp)] .= y
+    end
+    if zl != nothing
+        MadNLP.full(solver.zl)[1:get_nvar(nlp)] .= zl
+    end
+    if zu != nothing
+        MadNLP.full(solver.zu)[1:get_nvar(nlp)] .= zu
+    end
+
+    if !isempty(kwargs)
+        @warn(solver.logger, "The options set during resolve may not have an effect")
+        set_options!(solver.opt, kwargs)
+    end
+
+    try
+        if solver.status == MadNLP.INITIAL
+            MadNLP.@notice(
+                solver.logger,
+                "This is $(MadNLP.introduce()), using MadMPEC extension, running with $(MadNLP.introduce(solver.kkt.linear_solver))\n"
+            )
+            MadNLP.print_init(solver)
+            solver.status = MadNLP.initialize!(solver)
+        else # resolving the problem
+            solver.status = MadNLP.reinitialize!(solver)
+        end
+
+        while solver.status >= MadNLP.REGULAR
+            solver.status == MadNLP.REGULAR && (solver.status = MadMPEC.homotopy!(solver))
+            solver.status == MadNLP.RESTORE && (solver.status = MadNLP.restore!(solver))
+            solver.status == MadNLP.ROBUST && (solver.status = MadNLP.robust!(solver))
+        end
+    catch e
+        if e isa MadNLP.InvalidNumberException
+            if e.callback == :obj
+                solver.status=MadNLP.INVALID_NUMBER_OBJECTIVE
+            elseif e.callback == :grad
+                solver.status=MadNLP.INVALID_NUMBER_GRADIENT
+            elseif e.callback == :cons
+                solver.status=MadNLP.INVALID_NUMBER_CONSTRAINTS
+            elseif e.callback == :jac
+                solver.status=MadNLP.INVALID_NUMBER_JACOBIAN
+            elseif e.callback == :hess
+                solver.status=MadNLP.INVALID_NUMBER_HESSIAN_LAGRANGIAN
+            else
+                solver.status=MadNLP.INVALID_NUMBER_DETECTED
+            end
+        elseif e isa MadNLP.NotEnoughDegreesOfFreedomException
+            solver.status=MadNLP.NOT_ENOUGH_DEGREES_OF_FREEDOM
+        elseif e isa MadNLP.LinearSolverException
+            solver.status=MadNLP.ERROR_IN_STEP_COMPUTATION;
+            solver.opt.rethrow_error && rethrow(e)
+        elseif e isa InterruptException
+            solver.status=MadNLP.USER_REQUESTED_STOP
+            solver.opt.rethrow_error && rethrow(e)
+        else
+            solver.status=INTERNAL_ERROR
+            solver.opt.rethrow_error && rethrow(e)
+        end
+    finally
+        solver.cnt.total_time = time() - solver.cnt.start_time
+        if !(solver.status < MadNLP.SOLVE_SUCCEEDED)
+            MadNLP.print_summary(solver)
+        end
+        MadNLP.@notice(
+            solver.logger,
+            "EXIT: $(MadNLP.get_status_output(solver.status, solver.opt))"
+        )
+        solver.opt.disable_garbage_collector && (
+            GC.enable(true);
+            @warn(solver.logger, "Julia garbage collector is turned back on")
+        )
+        MadNLP.finalize(solver.logger)
+
+        MadNLP.update!(stats, solver)
+    end
+
+    return stats
+end
+
+function homotopy!(
     solver::MadNLP.MadNLPSolver{
         T,
         VT,
