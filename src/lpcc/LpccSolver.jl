dfa8b0a2af535ceb4f1a9e91b327bdb1c1052a42
Anton Pozharskiy <apozharski@gmail.com>
2025-07-07 11:01:14 +0200

    Highs solver

--- src/lpcc/LpccSolver.jl
+++ src/lpcc/LpccSolver.jl
@@ -36,7 +36,7 @@ struct LpccMILP{T, VT, MT}
     uba::VT
     lbx::VT
     ubx::VT
-    integrality::Vector{Bool}
+    integrality::Vector{Int} # kContinuous = 0, kInteger = 1
 
     function LpccMILP(mpcc::AbstractMPCCModel{T, VT}) where {T, VT}
         # TODO(@anton) we assume vertical form
@@ -82,8 +82,8 @@ struct LpccMILP{T, VT, MT}
         ubx = VT(undef, n)
         integrality = Vector{Bool}(undef, nvar+ncc)
 
-        integrality[1:nvar] .= false
-        integrality[(nvar+1):(nvar+ncc)] .= true
+        integrality[1:nvar] .= 0
+        integrality[(nvar+1):(nvar+ncc)] .= 1
 
         return new{T, VT, typeof(A)}(
             mpcc,
@@ -105,4 +105,76 @@ struct LpccMILP{T, VT, MT}
     end
 end
 
-function linearize(lpcc::LpccMILP, x::AbstractVector) end
+function linearize!(lpcc::LpccMILP, x::AbstractVector; tr=1e-1)
+    mpcc = lpcc.mpcc
+    # Update gradient
+    grad!(mpcc, x, lpcc.c)
+    # Update COO:
+    @views jac_coord!(mpcc, x, lpcc.avals[1:mpcc.meta.nnzj])
+    # Rebuild Sparse Matrix:
+    # TODO(@anton) is there a better way of doing this through the HIGHS interface itself?
+    m = mpcc.meta.ncon+2*mpcc.meta.ncc
+    n = mpcc.meta.nvar+mpcc.meta.ncc
+    SparseArrays.sparse!(
+        lpcc.arows,
+        lpcc.acols,
+        lpcc.avals,
+        m,
+        n,
+        +,
+        lpcc.klasttouch,
+        lpcc.csrrowptr,
+        lpcc.csrcolval,
+        lpcc.csrnzval,
+        lpcc.A.colptr,
+        lpcc.A.rowval,
+        lpcc.A.nzval,
+    )
+
+    # Apply trust region
+    ind_x = mpcc.meta.ind_x
+    ind_cc1 = mpcc.meta.ind_cc1
+    ind_cc2 = mpcc.meta.ind_cc2
+    @views begin
+        # Lower bound on x0
+        lpcc.lbx[ind_x] .= max.(mpcc.meta.lvar[ind_x] .- x[ind_x], -tr)
+        # Lower bound on x1 and x2
+        lpcc.lbx[ind_cc1] .= max.(mpcc.meta.lvar[ind_cc1] .- x[ind_cc1], -tr)
+        lpcc.lbx[ind_cc2] .= max.(mpcc.meta.lvar[ind_cc2] .- x[ind_cc2], -tr)
+        # upper bound on all
+        lpcc.ubx[1:mpcc.meta.nvar] .= min.(mpcc.meta.uvar .- x, tr)
+    end
+    lpcc.lbx[mpcc.meta.nvar:end] .= 0
+    lpcc.ubx[mpcc.meta.nvar:end] .= 1
+
+    return lpcc
+end
+
+function solve_with_highs(lpcc::LpccMILP)
+    # TODO(@anton) this is inefficient as we build the HiGHS solver each time,
+    # However, the HiGHs interface doesn't allow for efficiently updating the A matrix
+    # via just passing e.g. a new values array.
+
+    highs = Highs_create()
+    # Add variables
+    Highs_addVars(highs, length(lpcc.lbx), lpcc.lbx, lpcc.ubx)
+    # TODO(@anton) no need for comprehensions here, (or actually storing A.
+    #              we could just store colptr, rowval, and nzval, and modify them in linearze!
+    # Add constraints
+    Highs_addRows(
+        highs,
+        lpcc.A.m,
+        lpcc.lba,
+        lpcc.uba,
+        length(lpcc.A.nzval),
+        [i-1 for i in lpcc.A.colptr[1:lpcc.A.n]],
+        [i-1 for i in lpcc.A.rowval],
+        lpcc.A.nzval,
+    )
+    # Add objective
+    Highs_changeColsCostByRange(highs, 0, lpcc.A.n-1, lpcc.c)
+    # Set integrality
+    Highs_changeColsIntegralityByRange(highs, 0, lpcc.A.n-1, lpcc.integrality)
+
+    return Highs_run(highs)
+end
