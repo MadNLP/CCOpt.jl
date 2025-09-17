04fcd5a1a09cb9ce2c57e3c5772648d6c5b43978
Anton Pozharskiy <apozharski@gmail.com>
2025-09-17 12:50:12 +0200

    some work on linearization, begin on BigM model and Milp solver

--- src/lpcc/LpccSolver.jl
+++ src/lpcc/LpccSolver.jl
@@ -115,13 +115,55 @@ struct LpccMILP{T, VT, MT, ST}
     end
 end
 
-@kwdef struct LpccMILPOptions{T} <: AbstractLpccSolverOptions{T}
+# define milp solver for mpccs
+@kwdef struct MilpSolverOptions{T} <: AbstractLpccSolverOptions{T}
     verbose::Bool = true
     abs_gap::T = 1e-9
     rel_gap::T = 1e-4
     opt_tol::T = 1e-8
     feas_tol::T = 1e-8
     int_feas_tol::T = 1e-8
+    M::T = 1000.0
+    solver_type::Type = Gurobi.Optimizer
+end
+
+mutable struct MilpSolver{T, VT}
+    lpcc::AbstractMPCCModel{T, VT}
+    opts::MilpSolverOptions{T}
+    model::Model
+
+    function MilpSolver(
+        lpcc::AbstractMPCCModel{T, VT},
+        opts::MilpSolverOptions{T},
+    ) where {T, VT}
+        model = Model(opts.solver_type)
+        nvar = lpcc.meta.nvar
+        ncon = lpcc.meta.ncon
+        ncc = lpcc.meta.ncc
+
+        # TODO(@anton) Improvements to QuadraticModels, QuadraticModelsGurobi, and the creation of
+        #              QuadraticModelsHighs, would make this indirection through JuMP unnecessary.
+        #              This is a decent amount of work though :(
+        @variable(
+            model,
+            lpcc.meta.lbx[i] <= x[i=1:length(lpcc.meta.lbx)] <= lpcc.meta.ubx[i]
+        )
+        @objective(
+            model,
+            lpcc.meta.minimize ? MIN_SENSE : MAX_SENSE,
+            sum(lpcc.nlp.data.c .* x)
+        )
+        @constraint(model, lpcc.lba .<= lpcc.A * x .<= lpcc.uba)
+        for ii in (nvar+1):(nvar+ncc)
+            JuMP.set_binary(x[ii])
+        end
+
+        MOI.set.(model, MOI.VariablePrimalStart(), x[1:nvar], lpcc.meta.x0)
+        solver = new{T, VT}(lpcc, opts, model)
+        set_opts!(solver)
+
+        return solver
+    end
 end
 
 function linearize!(lpcc::LpccMILP, x::AbstractVector; tr=1e-1, presolve_binaries=true)
@@ -245,40 +287,52 @@ function tr!(
     end
 end
 
-function set_opts!(
-    model::Model,
-    lpcc::LpccMILP{T, VT, MT, Gurobi.Optimizer},
-    opts::LpccMILPOptions{T},
-) where {T, VT, MT}
-    MOI.set(model, MOI.Silent(), !opts.verbose)
-    JuMP.set_optimizer_attribute(model, "FeasibilityTol", opts.feas_tol)
-    JuMP.set_optimizer_attribute(model, "IntFeasTol", opts.int_feas_tol)
-    JuMP.set_optimizer_attribute(model, "OptimalityTol", opts.opt_tol)
-    JuMP.set_optimizer_attribute(model, "MIPGap", opts.rel_gap)
-    JuMP.set_optimizer_attribute(model, "MIPFocus", 1)
-    JuMP.set_optimizer_attribute(model, "Presolve", 1)
-    return JuMP.set_optimizer_attribute(model, "MIPGapAbs", opts.abs_gap)
+function set_opts!(solver::MilpSolver{T, VT}) where {T, VT}
+    model = solver.model
+    opts = solver.opts
+    if opts.solver_type == Gurobi.Optimizer
+        MOI.set(model, MOI.Silent(), !opts.verbose)
+        JuMP.set_optimizer_attribute(model, "FeasibilityTol", opts.feas_tol)
+        JuMP.set_optimizer_attribute(model, "IntFeasTol", opts.int_feas_tol)
+        JuMP.set_optimizer_attribute(model, "OptimalityTol", opts.opt_tol)
+        JuMP.set_optimizer_attribute(model, "MIPGap", opts.rel_gap)
+        JuMP.set_optimizer_attribute(model, "MIPFocus", 1)
+        JuMP.set_optimizer_attribute(model, "Presolve", 1)
+        JuMP.set_optimizer_attribute(model, "MIPGapAbs", opts.abs_gap)
+    elseif opts.solver_type == HiGHS.Optimizer
+        MOI.set(model, MOI.Silent(), !opts.verbose)
+        JuMP.set_optimizer_attribute(model, "kkt_tolerance", opts.feas_tol)
+        JuMP.set_optimizer_attribute(model, "mip_feasiblility_tolerance", opts.int_feas_tol)
+        JuMP.set_optimizer_attribute(model, "optimality_tolerance", opts.opt_tol)
+        JuMP.set_optimizer_attribute(model, "mip_rel_gap", opts.rel_gap)
+        JuMP.set_optimizer_attribute(model, "mip_abs_gap", opts.abs_gap)
+    end
+    return nothing
 end
 
-function set_opts!(
-    model::Model,
-    lpcc::LpccMILP{T, VT, MT, HiGHS.Optimizer},
-    opts::LpccMILPOptions{T},
-) where {T, VT, MT}
-    MOI.set(model, MOI.Silent(), !opts.verbose)
-    JuMP.set_optimizer_attribute(model, "kkt_tolerance", opts.feas_tol)
-    JuMP.set_optimizer_attribute(model, "mip_feasiblility_tolerance", opts.int_feas_tol)
-    JuMP.set_optimizer_attribute(model, "optimality_tolerance", opts.opt_tol)
-    JuMP.set_optimizer_attribute(model, "mip_rel_gap", opts.rel_gap)
-    return JuMP.set_optimizer_attribute(model, "mip_abs_gap", opts.abs_gap)
-end
+function build(solver::MilpSolver{T, VT}; y0=nothing) where {T, VT}
+    lpcc = solver.lpcc
+    opts = solver.opts
 
-function build(
-    lpcc::LpccMILP{T, VT, MT, ST},
-    opts::LpccMILPOptions{T};
-    x0=nothing,
-) where {T, VT, MT, ST}
-    model = Model(ST)
+    model = Model(opts.solver_type)
+    nvar = lpcc.meta.nvar
+    ncon = lpcc.meta.ncon
+    ncc = lpcc.meta.ncc
+
+    # TODO(@anton) Improvements to QuadraticModels, QuadraticModelsGurobi, and the creation of
+    #              QuadraticModelsHighs, would make this indirection through JuMP unnecessary.
+    #              This is a decent amount of work though :(
+    @variable(model, lpcc.meta.lbx[i] <= x[i=1:length(lpcc.meta.lbx)] <= lpcc.meta.ubx[i])
+    @objective(model, lpcc.meta.minimize ? MIN_SENSE : MAX_SENSE, sum(lpcc.nlp.data.c .* x))
+    @constraint(model, lpcc.lba .<= lpcc.A * x .<= lpcc.uba)
+
+    for ii in (nvar+1):(nvar+ncc)
+        JuMP.set_binary(x[ii])
+    end
+
+    MOI.set.(model, MOI.VariablePrimalStart(), x[1:nvar], lpcc.meta.x0)
+    set_opts!(solver)
+    solver.model = Model(ST)
     set_opts!(model, lpcc, opts)
 
     @variable(model, lpcc.lbx[i] <= x[i=1:length(lpcc.lbx)] <= lpcc.ubx[i])
@@ -288,8 +342,8 @@ function build(
         lpcc.integrality[ii] == one(Int32) && JuMP.set_binary(x[ii])
     end
 
-    if !isnothing(x0)
-        MOI.set.(model, MOI.VariablePrimalStart(), x, x0)
+    if !isnothing(y0)
+        MOI.set.(model, MOI.VariablePrimalStart(), x[], x0)
     end
     return model
 end
--- src/models/LPCCModel.jl
+++ src/models/LPCCModel.jl
@@ -1,10 +1,53 @@
+######################### types #########################
+abstract type AbstractLPCCModel{T, VT} <: AbstractMPCCModel{T, VT} end
+
+struct LPCCModel{T, VT} <: AbstractLPCCModel{T, VT}
+    nlp::NLPModels.AbstractNLPModel{T, VT}
+    meta::MPCCModelMeta{T, VT}
+    fixed_map::Vector{Int}
+end
+
 ######################### linearize #########################
+# TODO(@anton) cleanup allocations here.
 function linearize(mpcc::AbstractMPCCModel{T, VT}, x::VT; tr=0.0) where {T, VT}
+    if !is_vertical(mpcc)
+        error("linearization is supported for only vertical form MPCCs")
+    end
+    # Linearize the underiyling nlp
     lp = QuadraticModels.linearize(mpcc.nlp, x; tr=tr)
+
+    # Do trust region prunning
+    fixed_map = zeros(Int, mpcc.meta.ncc)
+    n_fixed = 0
+    if tr > 0.0
+        for ii in 1:mpcc.meta.ncc
+            icc1 = mpcc.meta.ind_cc1[ii]
+            icc2 = mpcc.meta.ind_cc2[ii]
+            # Assumes at least one branch is in the trust region
+            if x[icc1] - tr > mpcc.nlp.meta.lvar[icc1]
+                n_fixed += 1
+                fixed_map[ii] = 2
+                # update bounds in the lp
+                lp.meta.uvar[icc2] = lp.meta.lvar[icc2]
+                lp.meta.x0[icc2] = lp.meta.lvar[icc2]
+            elseif x[icc2] - tr > mpcc.nlp.meta.lvar[icc2]
+                n_fixed += 1
+                fixed_map[ii] = 1
+                # update bounds in the lp by fixing the variable
+                lp.meta.uvar[icc1] = lp.meta.lvar[icc1]
+                lp.meta.x0[icc1] = lp.meta.lvar[icc1]
+            end
+        end
+    end
+    # Prune the new ind_cc
+    ind_cc1 = mpcc.meta.ind_cc1[fixed_map .== 0]
+    ind_cc2 = mpcc.meta.ind_cc2[fixed_map .== 0]
+    cc_types = mpcc.meta.cc_types[fixed_map .== 0]
+    ind_x = setdiff(1:mpcc.meta.nvar, union(ind_cc1, ind_cc2))
 
     meta = MPCCModelMeta(
         Ref(lp.meta),
-        mpcc.meta.ncc,
+        mpcc.meta.ncc - n_fixed,
         mpcc.meta.ncon,
         mpcc.meta.ncon, # nlin
         0, # nnln
@@ -19,10 +62,10 @@ function linearize(mpcc::AbstractMPCCModel{T, VT}, x::VT; tr=0.0) where {T, VT}
         IndexSet(), # c_nln
         mpcc.meta.cc_l,
         mpcc.meta.cc_r,
-        mpcc.meta.ind_cc1,
-        mpcc.meta.ind_cc2,
-        mpcc.meta.cc_types,
-        mpcc.meta.ind_x,
+        ind_cc1,
+        ind_cc2,
+        cc_types,
+        ind_x,
         mpcc.meta.ind_c,
         mpcc.meta.ind_j_triplets,
         sort(union(mpcc.meta.ind_j_lin_triplets, mpcc.meta.ind_j_nln_triplets)), # ind_j_lin_triplets
@@ -35,7 +78,7 @@ function linearize(mpcc::AbstractMPCCModel{T, VT}, x::VT; tr=0.0) where {T, VT}
         Dict{Int, Int}(), #ind_j_nln_row_map
     )
 
-    return MPCCModel(lp, meta)
+    return LPCCModel(lp, meta, fixed_map)
 end
 
 ######################### linearize! #########################
@@ -46,6 +89,8 @@ function linearize!(
     x::VT;
     tr=0.0,
 ) where {T, VT}
+    # Reimplement linearize for quadratic model here because
+    # we need tighter control of the nlp meta
     QuadraticModels.linearize!(lpcc.nlp, mpcc.nlp, x; tr=tr)
 
     return lpcc
