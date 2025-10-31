34e4bdb76de118c4a8a259e46c924fbce6bb20f0
François Pacaud <frapac@users.noreply.github.com>
2025-10-31 12:04:07 +0100

    refactor structure of the package (#31)

--- src/MadMPEC.jl
+++ src/MadMPEC.jl
@@ -7,22 +7,24 @@ using Printf
 using DataStructures: CircularBuffer
 import MadNLP
 
-include("Types.jl")
+include("types.jl")
 include("enums.jl")
 include("MPCCModelMeta.jl")
 include("MPCCModel.jl")
 include("MPCCRelaxationModel.jl")
-include("ScholtesRelaxation.jl")
-include("FischerBurmeisterRelaxation.jl")
-include("ChenChenKanzowRelaxation.jl")
-include("NaturalResidualRelaxation.jl")
-include("BranchNLP.jl")
-include("models/Ell1Relaxation.jl")
-include("LiftedNLPModel.jl")
-include("solver/AbstractMPCCSolver.jl")
-include("solver/HomotopySolver.jl")
-include("lpcc/LpccSolver.jl")
-include("madnlpc/MadNLPC.jl")
-include("exact_penalty/ExactPenalty.jl")
+
+include("Models/scholtes_relaxation.jl")
+include("Models/fischer_burmeister_relaxation.jl")
+include("Models/chen_chen_kanzow_relaxation.jl")
+include("Models/natural_residual_relaxation.jl")
+include("Models/branch_nlp.jl")
+include("Models/l1_exact_penalty.jl")
+include("Models/lifted_model.jl")
+
+include("Solvers/AbstractMPCCSolver.jl")
+include("Solvers/homotopy_solver.jl")
+include("Solvers/lpcc_solver.jl")
+include("Solvers/madnlpc/MadNLPC.jl")
+include("Solvers/exact_penalty.jl")
 
 end # module MadMPEC
