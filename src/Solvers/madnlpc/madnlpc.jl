4c5ce2f8a8810862adc3651e153b14654d2d4bb6
Anton Pozharskiy <apozharski@gmail.com>
2026-02-07 09:02:12 +0100

    initial

--- src/Solvers/madnlpc/madnlpc.jl
+++ src/Solvers/madnlpc/madnlpc.jl
@@ -537,3 +537,175 @@ function update!(stats::MadNLPCExecutionStats, solver::MadNLPCSolver{T, VT}) whe
         stats.counters.counters.eval_function_time
     return stats
 end
+
+
+function regularize_Q!(solver::MadNLPCSolver{T}) where {T}
+    if solver.opts.kkt_regularization == :none || solver.ipm.mu < solver.opts.min_reg_mu
+        return false
+    end
+
+    ipm = solver.ipm
+    cb = ipm.cb
+    rnlp = solver.ell1
+    kkt = solver.ipm.kkt
+    n = length(ipm.x_ur)
+    ncc = solver.mpcc.meta.ncc
+    nnzh = solver.mpcc.meta.nnzh
+    rho = solver.ell1.tau[]
+    ind_cc1 = solver.mpcc.meta.ind_cc1
+    ind_cc2 = solver.mpcc.meta.ind_cc2
+    A = Array{T}(undef, 2, 2)
+    regularized = false
+    for i in 1:ncc
+        cc1 = ind_cc1[i]
+        cc2 = ind_cc2[i]
+        ys = MadNLP.slack(ipm.zu)[end-ncc+i]
+
+        if solver.opts.kkt_regularization == :eigenvalue_decomposition
+            A[1, 1] = kkt.pr_diag[cc1]
+            A[2, 2] = kkt.pr_diag[cc2]
+            A[2, 1] = ys*cb.obj_scale[] # TODO is this correct
+            A[1, 2] = ys*cb.obj_scale[]
+            E = eigen(Symmetric(A))
+            if E.values[1] < 0 || E.values[2] > solver.opts.max_eig_value
+                E.values[1] = solver.opts.min_eig_value
+                E.values[2] = min(solver.opts.max_eig_value, E.values[2])
+                A .= Symmetric(Matrix(E))
+                kkt.reg[cc1] = A[1, 1] - kkt.pr_diag[cc1]
+                kkt.reg[cc2] = A[2, 2] - kkt.pr_diag[cc2]
+                kkt.pr_diag[cc1] = A[1, 1]
+                kkt.pr_diag[cc2] = A[2, 2]
+                kkt.hess_raw.V[nnzh+i] = A[1, 2]
+                regularized = true
+            end
+        elseif solver.opts.kkt_regularization == :critical_rho
+            rho_max = sqrt(kkt.pr_diag[cc1]*kkt.pr_diag[cc2])
+            if rho*cb.obj_scale[] > rho_max
+                kkt.hess_raw.V[nnzh+i] =
+                    solver.opts.critical_rho_factor*rho_max*(
+                        rnlp.meta.minimize ? one(T) : -one(T)
+                    )
+                regularized = true
+            end
+        end
+    end
+    # We modify hess_raw so need to compress_hessian again.
+    MadNLP.compress_hessian!(kkt)
+    return regularized
+end
+
+function unregularize_Q!(solver::MadNLPCSolver{T}) where {T}
+    ipm = solver.ipm
+    cb = ipm.cb
+    rnlp = solver.ell1
+    kkt = solver.ipm.kkt
+    n = length(ipm.x_ur)
+    ncc = solver.mpcc.meta.ncc
+    nnzh = solver.mpcc.meta.nnzh
+    rho = cb.obj_scale[]*solver.ell1.tau[]
+    ind_cc1 = solver.mpcc.meta.ind_cc1
+    ind_cc2 = solver.mpcc.meta.ind_cc2
+    A = Array{T}(undef, 2, 2)
+    regularized = false
+    kkt.pr_diag[ind_cc1] .-= kkt.reg[ind_cc1]
+    kkt.pr_diag[ind_cc2] .-= kkt.reg[ind_cc2]
+    kkt.reg[ind_cc1] .= 0
+    kkt.reg[ind_cc2] .= 0
+    kkt.hess_raw.V[(nnzh+1):(nnzh+ncc)] .= rnlp.meta.minimize ? rho : -rho
+    # We modify hess_raw so need to compress_hessian again.
+    MadNLP.compress_hessian!(kkt)
+    return regularized
+end
+
+function MadNLP.inertia_correction!(
+    inertia_corrector::MadNLP.InertiaBased,
+    solver::MadNLPCSolver{T},
+) where {T}
+    ipm = solver.ipm
+    n_trial = 0
+    ipm.del_w = del_w_prev = zero(T)
+    ipm.del_c = del_c_prev = zero(T)
+
+    MadNLP.@trace(ipm.logger, "Inertia-based regularization started.")
+
+    MadNLP.factorize_wrapper!(ipm)
+    num_pos, num_zero, num_neg = MadNLP.inertia(ipm.kkt.linear_solver)
+
+    solve_status = if MadNLP.is_inertia_correct(ipm.kkt, num_pos, num_zero, num_neg)
+        # Try a backsolve. If the factorization has failed, solve_refine_wrapper returns false.
+        MadNLP.solve_refine_wrapper!(ipm.d, ipm, ipm.p, ipm._w4)
+    else
+        false
+    end
+
+    # Try to regularize
+    if !solve_status && regularize_Q!(solver)
+        MadNLP.@trace(ipm.logger, "Trying to regularize out penalty.")
+        MadNLP.factorize_wrapper!(ipm)
+        num_pos, num_zero, num_neg = MadNLP.inertia(ipm.kkt.linear_solver)
+
+        solve_status = if MadNLP.is_inertia_correct(ipm.kkt, num_pos, num_zero, num_neg)
+            # Try a backsolve. If the factorization has failed, solve_refine_wrapper returns false.
+            MadNLP.solve_refine_wrapper!(ipm.d, ipm, ipm.p, ipm._w4)
+        else
+            false
+        end
+
+        if !solve_status
+            # undo what we did and just do inertia correction
+            unregularize_Q!(solver)
+        end
+    end
+    while !solve_status
+        MadNLP.@debug(ipm.logger, "Primal-dual perturbed.")
+
+        if n_trial == 0
+            ipm.del_w =
+                ipm.del_w_last==zero(T) ? ipm.opt.first_hessian_perturbation :
+                max(
+                    ipm.opt.min_hessian_perturbation,
+                    ipm.opt.perturb_dec_fact*ipm.del_w_last,
+                )
+        else
+            ipm.del_w *=
+                ipm.del_w_last==zero(T) ? ipm.opt.perturb_inc_fact_first :
+                ipm.opt.perturb_inc_fact
+            if ipm.del_w>ipm.opt.max_hessian_perturbation
+                ipm.cnt.k+=1
+                MadNLP.@debug(
+                    ipm.logger,
+                    "Primal regularization is too big. Switching to restoration phase."
+                )
+                return false
+            end
+        end
+        ipm.del_c =
+            num_zero == 0 ? zero(T) :
+            ipm.opt.jacobian_regularization_value *
+            ipm.mu^(ipm.opt.jacobian_regularization_exponent)
+        MadNLP.regularize_diagonal!(ipm.kkt, ipm.del_w - del_w_prev, ipm.del_c - del_c_prev)
+        del_w_prev = ipm.del_w
+        del_c_prev = ipm.del_c
+
+        MadNLP.factorize_wrapper!(ipm)
+        num_pos, num_zero, num_neg = MadNLP.inertia(ipm.kkt.linear_solver)
+
+        solve_status = if MadNLP.is_inertia_correct(ipm.kkt, num_pos, num_zero, num_neg)
+            MadNLP.solve_refine_wrapper!(ipm.d, ipm, ipm.p, ipm._w4)
+        else
+            false
+        end
+
+        n_trial += 1
+    end
+
+    ipm.del_w != 0 && (ipm.del_w_last = ipm.del_w)
+    return true
+end
+
+function MadNLP.inertia_correction!(
+    inertia_corrector::MadNLP.AbstractInertiaCorrector,
+    solver::MadNLPCSolver{T},
+) where {T}
+    return MadNLP.inertia_correction!(inertia_corrector, solver.ipm)
+end
