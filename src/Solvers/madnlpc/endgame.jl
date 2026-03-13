3231fb76b86c5391b9e1e2f99bbde352aac8010f
Anton Pozharskiy <apozharski@gmail.com>
2026-03-13 09:59:18 +0100

    factor out dual magic steps

--- src/Solvers/madnlpc/endgame.jl
+++ src/Solvers/madnlpc/endgame.jl
@@ -22,6 +22,42 @@ function get_delta_candidate(nu, x, sigma, delta_max)
     end
 end
 
+# Calculate the new duals for a given relaxation
+function calculate_relax_magic_step(x, x_other, z, zs, delta, nu, nu_other, mu)
+    mu_r = mu + (x*z - mu)
+    z_hat = inv(x+delta)*mu_r # TODO(@anton): maybe do mu+r where r is the old residual
+    zs_hat = inv(x_other)*(-nu + z_hat) # TODO(@anton): if this doesn't work then calculate ther real residual instead of -nu1.
+    z_other_hat = x*zs_hat + nu_other # TODO(@anton) same here
+
+    return z_hat, z_other_hat, zs_hat
+end
+
+function set_relax_magic_step!(solver, ii, z1, z2, zs, x1, x2, delta_zs)
+    cc1 = solver.ind_cc1[ii]
+    cc2 = solver.ind_cc2[ii]
+    ncc = get_ncc(mpcc)
+    ipm = solver.ipm
+    cb = ipm.cb
+
+    ## TODO(@anton) Set the new slack?
+    ## TODO(@anton) also adjust x1?
+    #MadNLP.slack(ipm.x)[end-ncc+ii] = -mu*inv(zs_hat)
+    ## Set the new duals
+    MadNLP.variable(ipm.zl)[cc1] = z1
+    MadNLP.variable(ipm.zl)[cc2] = z2
+    MadNLP.slack(ipm.zu)[end-ncc+ii] = zs
+    ipm.y[end-ncc+ii] = zs
+
+    ## Set the new J'y_c
+    # TODO(@anton) is this correct
+    ipm.jacl[cc1] += x2*delta_zs*cb.con_scale[end-ncc+ii]
+    ipm.jacl[cc2] += x1*delta_zs*cb.con_scale[end-ncc+ii]
+    ipm.jacl[end-ncc+ii] -= delta_zs*cb.con_scale[end-ncc+ii]
+
+    ## Set the multiplier contribution in the Hessian of the Lagrangian
+    return ipm.kkt.hess[end-ncc+ii] = zs_hat*cb.con_scale[end-ncc+ii]
+end
+
 # If using Scholtes do LB relaxation
 function do_endgame!(
     solver::MadNLPCSolver{T},
@@ -72,31 +108,13 @@ function do_endgame!(
             MadNLP.variable(ipm.xl)[cc1] = get_lvar(mpcc)[cc1_orig] - rnlp.δ1[ii]
 
             # Calculate new values
-            mu_r = mu + (x1*z1 - mu)
-            nu_res = -MadNLP.primal(ipm.f)[cc1] - MadNLP.primal(ipm.zu)[cc1]
-            z1_hat = inv(x1+rnlp.δ1[ii])*mu_r # TODO(@anton): maybe do mu+r where r is the old residual
-            zs_hat = inv(x2)*(-nu1 + z1_hat) # TODO(@anton): if this doesn't work then calculate ther real residual instead of -nu1.
-            z2_hat = x1*zs_hat + nu2 # TODO(@anton) same here
+            z1_hat, z2_hat, zs_hat =
+                calculate_relax_magic_step(x1, x2, z1, zs, delta_candidate, nu1, nu2, mu)
             delta_zs = zs_hat - zs
 
             # Set new values
-            ## TODO(@anton) Set the new slack?
-            ## TODO(@anton) also adjust x1?
-            #MadNLP.slack(ipm.x)[end-ncc+ii] = -mu*inv(zs_hat)
             ## Set the new duals
-            MadNLP.variable(ipm.zl)[cc1] = z1_hat
-            MadNLP.variable(ipm.zl)[cc2] = z2_hat
-            MadNLP.slack(ipm.zu)[end-ncc+ii] = zs_hat
-            ipm.y[end-ncc+ii] = zs_hat
-
-            ## Set the new J'y_c
-            # TODO(@anton) is this correct
-            ipm.jacl[cc1] += x2*delta_zs*cb.con_scale[end-ncc+ii]
-            ipm.jacl[cc2] += x1*delta_zs*cb.con_scale[end-ncc+ii]
-            ipm.jacl[end-ncc+ii] -= delta_zs*cb.con_scale[end-ncc+ii]
-
-            ## Set the multiplier contribution in the Hessian of the Lagrangian
-            ipm.kkt.hess[end-ncc+ii] = zs_hat*cb.con_scale[end-ncc+ii]
+            set_relax_magic_step!(solver, ii, z1_hat, z2_hat, zs_hat, x1, x2, delta_zs)
         end
 
         nu2_inactive = endgame.use_filtered ? nu2_filt <= -(nu_bound) : nu2 <= -(nu_bound)
@@ -112,30 +130,12 @@ function do_endgame!(
             MadNLP.variable(ipm.xl)[cc2] = get_lvar(mpcc)[cc2_orig] - rnlp.δ2[ii]
 
             # Calculate new values
-            mu_r = mu + (x2*z2 - mu)
-            nu_res = -MadNLP.primal(ipm.f)[cc2] - MadNLP.primal(ipm.zu)[cc2]
-            px = MadNLP.primal(ipm.p)
-            z2_hat = inv(x2+rnlp.δ2[ii])*mu_r # TODO(@anton): maybe do mu+r where r is the old residual
-            zs_hat = inv(x1)*(-nu2 + z2_hat) # TODO(@anton): if this doesn't work then calculate ther real residual instead of -nu1.
-            z1_hat = x2*zs_hat + nu1 # TODO(@anton) same here
+            z2_hat, z1_hat, zs_hat =
+                calculate_relax_magic_step(x2, x1, z2, zs, delta_candidate, nu2, nu1, mu)
             delta_zs = zs_hat - zs
+
             # Set new values
-            ## Set the new slack?
-            #MadNLP.slack(ipm.x)[end-ncc+ii] = -mu*inv(zs_hat)
-            ## Set the new duals
-            MadNLP.variable(ipm.zl)[cc1] = z1_hat
-            MadNLP.variable(ipm.zl)[cc2] = z2_hat
-            MadNLP.slack(ipm.zu)[end-ncc+ii] = zs_hat
-            ipm.y[end-ncc+ii] = zs_hat
-
-            #MadNLP.jtprod!(ipm.jacl, ipm.kkt, ipm.y)
-            ## Set the new J'y_c
-            ipm.jacl[cc1] += x2*delta_zs*cb.con_scale[end-ncc+ii]
-            ipm.jacl[cc2] += x1*delta_zs*cb.con_scale[end-ncc+ii]
-            ipm.jacl[end-ncc+ii] -= delta_zs*cb.con_scale[end-ncc+ii]
-
-            ## Set the multiplier contribution in the Hessian of the Lagrangian
-            ipm.kkt.hess[end-ncc+ii] = zs_hat*cb.con_scale[end-ncc+ii]
+            set_relax_magic_step!(solver, ii, z1_hat, z2_hat, zs_hat, x1, x2, delta_zs)
         end
     end
 
