9422c5a12db1604fa435f5bb4c9085842658e47e
Anton Pozharskiy <apozharski@gmail.com>
2026-07-21 12:02:19 +0200

    move scripts to experiments repo

--- scripts/casadi/casadi_mpcc.jl
+++
@@ -1,8 +0,0 @@
-using CCOpt, CasADiNLPModels, JSON
-
-function CasADiMPCCModel(libpath::String, datapath::String)
-    nlp = CasADiNLPModel(libpath, datapath)
-    data = JSON.parsefile(datapath; allownan=true)
-    mpcc = MPCCModelConCon(nlp, Vector{Int}(data["ind_cc1"]), Vector{Int}(data["ind_cc2"]))
-    return vertical_form(mpcc)
-end
