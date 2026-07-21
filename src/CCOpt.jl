02ef343e23f222c9949775cb17cd0835624bc7e2
Anton Pozharskiy <apozharski@gmail.com>
2026-07-21 10:53:12 +0200

    move experiments to separate repo

--- src/CCOpt.jl
+++ src/CCOpt.jl
@@ -12,12 +12,8 @@ include("MPCCRelaxationModel.jl")
 include("MPCCPenaltyModel.jl")
 
 include("Models/scholtes_relaxation.jl")
-include("Models/fischer_burmeister_relaxation.jl")
-include("Models/chen_chen_kanzow_relaxation.jl")
-include("Models/natural_residual_relaxation.jl")
 include("Models/branch_nlp.jl")
 include("Models/l1_exact_penalty.jl")
-include("Models/lasry_lions_penalty.jl")
 include("Models/big_m_model.jl")
 
 include("utils.jl")
